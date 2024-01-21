9212f311d192cc1ba4e7a02f17bd260f4a38c4dd
Yassir aittata <yassir.aittata@gmail.com>
2024-01-21 19:29:42 +0000

    .

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -4,8 +4,4 @@ import { OrdersService } from './orders.service';
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
-
-  @Post()
-  creatOrder() {
-  }
 }
